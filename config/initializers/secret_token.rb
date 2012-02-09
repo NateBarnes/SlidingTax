2fed20c2b7c539a9fc9fd3b3bf7893816419192b
Nathaniel Barnes <kerianambrai@gmail.com>
2012-02-09 16:45:13 -0600

    Added a basic rails application strucutre

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slidingtax::Application.config.secret_token = '189696c1c328925dcdbb3a36b7c7d8132c42bc882d81b1e1dac054d1c1fe91c705708206cf1b78834edf7fdc25e44283a2cab93c3e7da430663b32b4370bf836'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Slidingtax::Application.config.session_store :cookie_store, key: '_slidingtax_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Slidingtax::Application.config.session_store :active_record_store
